9adf47c1fb41e58a5a95a64ec5505a574cbda039
Yevhen Kariev <yevhenkariev@gmail.com>
2025-10-21 12:43:25 +0200

    user controller

---
+++ src/main/java/org/kariev/inventorymgtsystem/controllers/AuthController.java
@@ -0,0 +1,30 @@
+package org.kariev.inventorymgtsystem.controllers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.kariev.inventorymgtsystem.dtos.RegisterRequestDTO;
+import org.kariev.inventorymgtsystem.dtos.ResponseDTO;
+import org.kariev.inventorymgtsystem.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<ResponseDTO> register(@RequestBody @Valid RegisterRequestDTO request) {
+        return ResponseEntity.ok(service.registerUser(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ResponseDTO> login(@RequestBody @Valid RegisterRequestDTO request) {
+        return ResponseEntity.ok(service.registerUser(request));
+    }
+
+}
---
+++ src/main/java/org/kariev/inventorymgtsystem/controllers/UserController.java
@@ -0,0 +1,51 @@
+package org.kariev.inventorymgtsystem.controllers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.kariev.inventorymgtsystem.dtos.ResponseDTO;
+import org.kariev.inventorymgtsystem.dtos.UserDTO;
+import org.kariev.inventorymgtsystem.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService service;
+
+    @GetMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<ResponseDTO> getUsers() {
+        return ResponseEntity.ok(service.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN') or #id.toString() == authentication.principal.id")
+    public ResponseEntity<ResponseDTO> getUserById(@PathVariable UUID id) {
+        return ResponseEntity.ok(service.getUserById(id));
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN') or #id.toString() == authentication.principal.id")
+    public ResponseEntity<ResponseDTO> updateUser(@PathVariable UUID id, @Valid @RequestBody UserDTO user) {
+        return ResponseEntity.ok(service.updateUser(id, user));
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN') or #id.toString() == authentication.principal.id")
+    public ResponseEntity<Void> deleteUser(@PathVariable UUID id) {
+        service.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}/transactions")
+    @PreAuthorize("hasAuthority('ADMIN') or #id.toString() == authentication.principal.id")
+    public ResponseEntity<ResponseDTO> getUserTransactions(@PathVariable UUID id) {
+        return ResponseEntity.ok(service.getUserTransactions(id));
+    }
+}
