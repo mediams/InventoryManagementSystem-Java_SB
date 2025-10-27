8de6bb396699ebab633db34d591468d2c6499baa
Yevhen Kariev <yevhenkariev@gmail.com>
2025-10-27 15:08:52 +0100

    transaction controller

---
+++ src/main/java/org/kariev/inventorymgtsystem/controllers/TransactionController.java
@@ -0,0 +1,65 @@
+package org.kariev.inventorymgtsystem.controllers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.kariev.inventorymgtsystem.dtos.ResponseDTO;
+import org.kariev.inventorymgtsystem.dtos.TransactionRequestDTO;
+import org.kariev.inventorymgtsystem.enums.TransactionStatus;
+import org.kariev.inventorymgtsystem.services.TransactionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/transactions")
+@RequiredArgsConstructor
+public class TransactionController {
+    private final TransactionService service;
+
+    @PostMapping("/purchase")
+    public ResponseEntity<ResponseDTO> purchase(@RequestBody @Valid TransactionRequestDTO request) {
+        return ResponseEntity.ok(service.purchaseTransaction(request));
+    }
+
+
+    @PostMapping("/sell")
+    public ResponseEntity<ResponseDTO> sell(@RequestBody @Valid TransactionRequestDTO request) {
+        return ResponseEntity.ok(service.sellTransaction(request));
+    }
+
+    @PostMapping("/return")
+    public ResponseEntity<ResponseDTO> returnTransaction(@RequestBody @Valid TransactionRequestDTO request) {
+        return ResponseEntity.ok(service.returnToSupplierTransaction(request));
+    }
+
+    @GetMapping()
+    public ResponseEntity<ResponseDTO> getAll(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "1000") int size,
+            @RequestParam(required = false) String search
+    ) {
+        return ResponseEntity.ok(service.getAllTransactions(page, size, search));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseDTO> getTransactionById(@PathVariable UUID id) {
+        return ResponseEntity.ok(service.getTransactionById(id));
+    }
+
+    @GetMapping("/by-month-and-year")
+    public ResponseEntity<ResponseDTO> getAllTransactionsByMonthAndYear(
+            @RequestParam int month,
+            @RequestParam int year
+    ) {
+        return ResponseEntity.ok(service.getAllTransactionsByMonthAndYear(month, year));
+    }
+
+    @PutMapping("/transactionId")
+    public ResponseEntity<ResponseDTO> updateTransaction(
+            @RequestParam UUID transactionId,
+            @RequestParam TransactionStatus transactionStatus
+    ) {
+        return ResponseEntity.ok(service.updateTransaction(transactionId, transactionStatus));
+    }
+}
